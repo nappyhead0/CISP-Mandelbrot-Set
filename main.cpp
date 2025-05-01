5fe44572c418a41bc5d072d6b1627e3e5796014d
nappyhead0 <115950509+nappyhead0@users.noreply.github.com>
2025-04-30 20:11:17 -0700

    Updated main.cpp with ryan's SFML skeleton

--- main.cpp
+++ main.cpp
@@ -1,1 +1,65 @@
+#include <SFML/Graphics.hpp>
 
+#include <iostream>
+
+
+
+
+using namespace sf;
+
+using namespace std;
+
+
+
+
+int main()
+
+{
+
+ int pixelWidth = VideoMode::getDesktopMode().width;
+
+ int pixelHeight = VideoMode::getDesktopMode().height;
+
+ VideoMode vm(pixelWidth, pixelHeight);
+
+ RenderWindow window(vm, "Rainbow Screen", Style::Default);
+
+
+
+
+ while (window.isOpen())
+
+ {
+
+ ///Input
+
+ if (Keyboard::isKeyPressed(Keyboard::Escape))
+
+ {
+
+ window.close();
+
+ }
+
+
+
+
+ ///Update
+
+
+
+
+ ///Draw
+
+ window.clear();
+
+ //window.draw(...);
+
+ window.display();
+
+ }
+
+
+
+
+}
